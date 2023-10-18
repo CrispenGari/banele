a0020cc7db7412e566f6e83abf2fc6caa3bc0f9b
Crispen Gari <crispengari@gmail.com>
2023-10-18 20:05:40 +0200

    eee

--- build.py
+++ build.py
@@ -1,9 +1,7 @@
 from app import app
 from flask_frozen import Freezer
 
-
-freezer = Freezer(app=app)
-freezer.freeze_yield()
+freezer = Freezer(app=app, with_static_files=True)
 if __name__ == '__main__':
     freezer.freeze()
     
